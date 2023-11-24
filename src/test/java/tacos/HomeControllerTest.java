7bb32717ddc0181593fda383d5c0840cfd05dd10
ilshat16 <illliat@mail.ru>
2023-11-24 09:38:00 +0300

    add styles for errors and change HomeController class

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -11,9 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import tacos.controllers.HomeController;
+import tacos.controllers.WebConfig;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest(WebConfig.class)
 public class HomeControllerTest {
 	
 	@Autowired
