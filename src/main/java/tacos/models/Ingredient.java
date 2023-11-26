eb37b69208155845331bb111b70cf8faaaac99b4
ilshat16 <illliat@mail.ru>
2023-11-26 16:30:51 +0300

    Realize spring data jpa

--- src/main/java/tacos/models/Ingredient.java
+++ src/main/java/tacos/models/Ingredient.java
@@ -1,15 +1,14 @@
 package tacos.models;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 public class Ingredient {
