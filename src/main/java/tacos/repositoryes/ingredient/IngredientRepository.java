030faa0059a36f70f88e737da687686445fb2c75
ilshat16 <illliat@mail.ru>
2023-11-26 14:29:13 +0300

    realize spring data jpa

--- src/main/java/tacos/repositoryes/ingredient/IngredientRepository.java
+++ src/main/java/tacos/repositoryes/ingredient/IngredientRepository.java
@@ -1,13 +1,8 @@
 package tacos.repositoryes.ingredient;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
 
 import tacos.models.Ingredient;
 
-public interface IngredientRepository {
-
-	List<Ingredient> findAll();
-	Optional<Ingredient> findById(String id);
-	Ingredient save(Ingredient ingredient);
+public interface IngredientRepository extends CrudRepository<Ingredient, String>{
 }
